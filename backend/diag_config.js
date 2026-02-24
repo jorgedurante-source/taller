862a6cd8f709981363b1c9ec46e910410df3a427
Jorge Durante <jorgedurante@gmail.com>
2026-02-24 19:04:57 -0300

    Cleanup diagnostic scripts

--- backend/diag_config.js
+++
@@ -1,7 +0,0 @@
-const Database = require('better-sqlite3');
-const path = require('path');
-const dbPath = path.join(__dirname, 'tenants/kabul/db.sqlite');
-const db = new Database(dbPath);
-const info = db.prepare("PRAGMA table_info('config')").all();
-console.log('Config info for kabul:', info.map(c => c.name));
-db.close();
